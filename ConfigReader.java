17bfe591c9100db0339435ee61df2e966a6e87a3
Katorly <70022443+katorly@users.noreply.github.com>
2021-08-08 22:14:03 +0800

    Update ConfigReader.java

--- ConfigReader.java
+++ ConfigReader.java
@@ -6,7 +6,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.List;
 
 public class ConfigReader {
     private File file;
